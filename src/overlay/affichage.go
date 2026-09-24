81257aede440be51c76a82dc874c1de0cd20eb6a
Romain Pornot <romainpornot@Romains-MacBook-Air.local>
2026-09-24 09:24:15 +0200

    Feat : Ajout que quand le choix du personnage et fais on puiise changeais sont prenom et que ca s'affiche dans le menu principal

--- src/overlay/affichage.go
+++ src/overlay/affichage.go
@@ -8,3 +8,9 @@ func PrintWalletOverlay(argent int, jetons int) {
 	fmt.Printf("\033[19;75H\033[44m\033[30m JETONS : %d \033[0m", jetons)
 	fmt.Print("\033[u")
 }
+
+func PrintFirstNameOverlay(firstName string) {
+	fmt.Print("\033[s")
+	fmt.Printf("\033[20;75H\033[46m\033[30m PRENOM : %-10s \033[0m", firstName)
+	fmt.Print("\033[u")
+}
